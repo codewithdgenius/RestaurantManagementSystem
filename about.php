2350d0220b3e83f66cb85aed3d9ad0d166c21ee2
Michael Humbo <ezekielmichaelhumbo98@gmail.com>
2022-12-08 05:30:44 +0000

    This is my first version of this project

---
+++ about.php
@@ -0,0 +1,187 @@
+<?php include "header.php"; ?>
+	<!-- Start header -->
+	<img src="images/banner_about_us.jpg" width="100%">
+	<!-- End header -->
+	
+	<!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">s
+			<div class="row">
+				<div class="col-lg-6 col-md-6 text-center">
+					<div class="inner-column">
+						<h1>Welcome To <span>Hackathon food and resturent</span></h1>
+						<h4>Little Story</h4>
+						<p> The Hackathon is an intellectual program that attracts design minds worldwide. It helps the participants to bring out their innovative ideas at the competing platform. </p>
+						<p> It helps the participants to bring out their innovative ideas at the competing platform. </p>
+						<!--<a class="btn btn-lg btn-circle btn-outline-new-white" href="#"> Comment</a> -->
+					</div>
+				</div>
+				<div class="col-lg-6 col-md-6">
+					<img src="images/images.jpg" style="height: 450px; width: 100%" class="img-fluid">
+				</div>
+				<div class="col-md-12">
+					<div class="inner-pt">
+						<p> Making way for a healthy meal is Hackathon food and Restaurant in aim. This place is synonymous with delicious food that can satiate all food cravings. It is home to some of the most appreciated citizen. So as to be able to cater to a large number of diners, it occupies a favourable location at the Northern Provience at the heart of Makeni city. 
+
+ </p>
+						<p>Hackathon food and Restaurant at Azzolini HighWay makes sure one has a great food experience by offering highly palatable food. The restaurant welcomes guests from 11:00-19:00 - 15:00-00:00 allowing diners to relish a scrumptious meal between the functional hours.The price range of the food at the restaurant ranges from 1. This listing is also listed in Restaurants,  Inexpensive Hackathon food and Restaurants (Below Le 300), we also do Delivery in this Restaurants.
+ </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
+	
+	<!-- Start Menu -->
+	<div class="menu-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Special Menu</h2>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+					</div>
+				</div>
+			</div>
+				
+			<div class="row inner-menu-box">
+				<div class="col-3">
+					<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+						<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">All</a>
+						<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Chinease</a>
+						<a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Contiental</</a>
+						<a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Rice</a>
+
+						<a class="nav-link" id="v-pills-tava-tab" data-toggle="pill" href="#v-pills-tava" role="tab" aria-controls="v-pills-tava" aria-selected="false">African</a>
+					</div>
+				</div>
+				
+				<div class="col-9">
+					<div class="tab-content" id="v-pills-tabContent">
+						<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+							<div class="row">
+								<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<p>Rs <?php echo $r['price']; ?> /-</p>
+											<h5 align="center">
+									<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+							</div>	
+						</div>
+
+
+						<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+							<div class="row">
+
+
+							<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='kathiyawadi'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center">
+												<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+
+
+
+											</h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+								
+								
+							</div>
+							
+						</div>
+						<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+							<div class="row">
+								
+								<?php include "connect.php";
+								$s = mysqli_query($con,"select * from menu where category='rajsthani'");
+								while($r = mysqli_fetch_array($s))
+								{	
+								?>
+								<div class="col-lg-4 col-md-6 special-grid drinks">
+									<div class="gallery-single fix">
+										<img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
+										<div class="why-text">
+											<h4><?php echo $r['title']; ?></h4>
+											<p><?php echo $r['description']; ?></p>
+											<h5>Rs <?php echo $r['price']; ?> /-</h5>
+											<h5 align="center">
+												<?php if(isset($_SESSION['uid']))
+									{
+									?>
+											<a href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add Cart</a></h5>
+
+									<?php
+									}
+									else
+									{
+									?>
+										<a href="login.php">Add Cart</a>
+									<?php } ?>
+
+
+											</h5>
+										</div>
+									</div>
+								</div>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>								
+
+	<!-- End Menu -->
+	
+	<?php include "footer.php" ; ?>
---
+++ contact.php
@@ -0,0 +1,64 @@
+<?php include "header.php"; ?>	
+	<!-- Start All Pages -->
+		
+		<img src="images/cn.png" width="100%">
+	<!-- End All Pages -->
+	
+	<!-- Start Contact -->
+	
+	<div>
+     <iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp=8.887085675329104~-12.064511775970459&lvl=16&typ=d&sty=r&src=SHELL&FORM=MBEDV8" scrolling="no">
+     </iframe>
+     <div style="white-space: nowrap; text-align: center; width: 500px; padding: 6px 0;">
+        <a id="largeMapLink" target="_blank" href="https://www.bing.com/maps?cp=8.887085675329104~-12.064511775970459&amp;sty=r&amp;lvl=16&amp;FORM=MBEDLD">View Larger Map</a> &nbsp; | &nbsp;
+        <a id="dirMapLink" target="_blank" href="https://www.bing.com/maps/directions?cp=8.887085675329104~-12.064511775970459&amp;sty=r&amp;lvl=16&amp;rtp=~pos.8.887085675329104_-12.064511775970459____&amp;FORM=MBEDLD">Get Directions</a>
+    </div>
+</div>
+
+	<div class="contact-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Contact</h2>
+						<p>Contact Information are Listed here Thanks for Visit Our Site</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<form id="contactForm">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<h1 style="font-family: cambria; color: red; text-align: center;">Hackathon food and Restaurent</h1>
+									<div class="help-block with-errors"></div>
+								</div>                                 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Email Us :</b> hackathon1@gmail.com</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Mobile  :</b> +23275385356</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group"> 
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Location  :</b> Azzolini HighWay</span></center>
+									<div class="help-block with-errors"></div>
+								</div>
+								
+							</div>
+						</div>            
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact -->
+<?php include "footer.php"; ?>
